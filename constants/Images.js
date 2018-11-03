dcde1e1c336a8b82b0cd28abe18ac720d0320faa
Dan McAtee <info@danmcatee.com>
2018-11-04 00:08:14 +0100

    add to theme constants

--- constants/Images.js
+++ constants/Images.js
@@ -1,5 +1,12 @@
 export const images = {
   splashScreen: require('../assets/images/splashScreen.png'),
+  homeHeader: require('../assets/images/home_header.png'),
+  homeSuxxess: require('../assets/images/home_suxxess.png'),
+  startPageArrow: require('../assets/images/start_page_segment.png'),
+  dailyInspirationOne: require('../assets/images/di_1.png'),
+  dailyInspirationTwo: require('../assets/images/di_2.png'),
+  dailyInspirationThree: require('../assets/images/di_3.png'),
+  newAtImotex: require('../assets/images/new_imotex.png'),
 };
 
 export const tabBarIcons = {
--- constants/Theme.js
+++ constants/Theme.js
@@ -2,5 +2,6 @@ export default {
   borderRadius: 2,
   colors: {
     grey: '#E9EBF1',
+    red: '#BD111F',
   },
 };
